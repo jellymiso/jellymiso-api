d3f60ce12cb15663bdb2392da76ff47f7110c6ac
jellymiso <astray03@gmail.com>
2021-07-07 02:42:18 +0800

    Signed-off-by: jellymiso <astray03@gmail.com>

--- modules/requestMail.js
+++ modules/requestMail.js
@@ -3,6 +3,7 @@
 //===============
 const sgMail = require('@sendgrid/mail');
 const funcHelper = require('../funcHelper');
+const https = require('https');
 
 //url format: /[api-module]/[api-name]/[params]
 //example: /mail/send/
@@ -41,24 +42,39 @@ function apiMail(response, request) {
                 request.on('end', function () {
 
                     post = JSON.parse(accumulatedData);
-
-                    sendMail(
-                        post.mailTo,
-                        post.mailFrom,
-                        post.mailSubject,
-                        post.mailText + "<br/><br/>" +
-                        "------------------<br/>" +
-                        "Message sent by: " + post.messageFromName + " (" + post.messageFromEmail + ") ",
-                        function () {
-                            //completed
-                            var retObj = {
-                                status: "success"
-                            }
-                            response.writeHead(200, { "Content-Type": "application/json" });
-                            response.end(JSON.stringify(retObj));
-                        },
-                        function (error) {
-                            //error
+                    
+                    validateRecaptcha(post.recaptchaToken)
+                    .then((data) => {
+                        console.log("data", data)
+                        if(data.success === true){
+                            sendMail(
+                                post.mailTo,
+                                post.mailFrom,
+                                post.mailSubject,
+                                post.mailText + "<br/><br/>" +
+                                "------------------<br/>" +
+                                "Message sent by: " + post.messageFromName + " (" + post.messageFromEmail + ") ",
+                                function () {
+                                    //completed
+                                    var retObj = {
+                                        status: "success"
+                                    }
+                                    response.writeHead(200, { "Content-Type": "application/json" });
+                                    response.end(JSON.stringify(retObj));
+                                },
+                                function (error) {
+                                    //error
+                                    var retObj = {
+                                        status: "error",
+                                        error_msg: error
+                                    }
+                                    response.writeHead(200, { "Content-Type": "application/json" });
+                                    response.end(JSON.stringify(retObj));
+                                }
+                            );
+                        }
+                        else{
+                            let error = "Failed reCaptcha verification!";
                             var retObj = {
                                 status: "error",
                                 error_msg: error
@@ -66,11 +82,16 @@ function apiMail(response, request) {
                             response.writeHead(200, { "Content-Type": "application/json" });
                             response.end(JSON.stringify(retObj));
                         }
-                    );
+                    })
+                    .catch((err) => {
+                        var retObj = {
+                            status: "error",
+                            error_msg: err
+                        }
+                        response.writeHead(400, { "Content-Type": "application/json" });
+                        response.end(JSON.stringify(retObj));
+                    })
                 });
-
-                // console.log("send module successfully ran");
-
             }
             else {
                 response.writeHead(404, { 'Content-Type': 'text/plain' });
@@ -119,6 +140,39 @@ function sendMail(sendTo, sendFrom, sendSubject, sendHtml, onComplete, onError) 
                 onError(error);
             }
         })
+}
+
+ function validateRecaptcha(token) {
+
+    let myRet = {
+        'success': false
+    };
+     //const secret = "6LcDVnsbAAAAAF3e7tScXim8Kd2JoOFY3fZ74Vqu"; //development
+    const secret = process.env.RECAPTCHA_SECRET_KEY; //production
+    const url = "https://www.google.com/recaptcha/api/siteverify?secret=" + secret + "&response=" + token
+    //
+
+    return new Promise((resolve, reject) => {
+        https.get(url, (req) => {
+
+            var accumulatedData = "";
+            var post = "";
+            req.on('data', function (chunk) {
+                accumulatedData += chunk;
+            });
+
+            req.on('end', function () {
+
+                post = JSON.parse(accumulatedData);
+                myRet = post;
+                resolve(myRet)
+
+            })
+        }).on("error", (err) => {
+            console.log("Error: " + err.message);
+            reject(myRet)
+        })
+    })
 }
 
 
